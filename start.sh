7ed5151015a35303465b06e28aa94ccbe0bb4a5a
hophuvinh <33288728+hophuvinh@users.noreply.github.com>
2026-03-09 00:55:30 +0700

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Run API server + Bot concurrently
-uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000} &
-API_PID=$!
-sleep 2  # wait for API to be ready
-python bot.py &
-BOT_PID=$!
-wait $API_PID $BOT_PID
